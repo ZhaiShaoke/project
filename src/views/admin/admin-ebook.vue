e581835c4ef3f7d96668ca60c56d8c05bd83d07b
zhai-shaoke <2509551708@qq.com>
2022-01-19 22:20:05 +0800

    feat:add the function of searching e-books directly by name

--- src/views/admin/admin-ebook.vue
+++ src/views/admin/admin-ebook.vue
@@ -4,9 +4,23 @@
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
       <p>
-        <a-button type="primary" @click="add()" size="large">
-          新增
-        </a-button>
+        <a-form layout="inline" :model="param"
+        >
+          <a-form-item>
+            <a-input v-model:value="param.name" placeholder="名称">
+            </a-input>
+          </a-form-item>
+          <a-form-item>
+            <a-button type="primary" @click="handleQuery({page:1,size:pagination.pageSize})">
+              查询
+            </a-button>
+          </a-form-item>
+          <a-form-item>
+             <a-button type="primary" @click="add()">
+                新增
+             </a-button>
+          </a-form-item>
+        </a-form>
       </p>
       <a-table
           :columns="columns"
@@ -78,10 +92,12 @@ const listData:any = [];
 export default defineComponent({
   name: 'AdminEbook',
   setup(){
-   const ebooks = ref()
+    const param = ref()
+    param.value = {}
+    const ebooks = ref()
     const pagination = ref({
       current:1,
-      pageSize:10,
+      pageSize:4,
       total:0
     })
 
@@ -133,7 +149,7 @@ export default defineComponent({
         params:{
           page:params.page,
           size:params.size,
-
+          name:param.value.name
         }
       }).then((response) =>{
         loading.value = false
@@ -218,12 +234,13 @@ export default defineComponent({
     });
 
     return{
+      param,
       ebooks,
       pagination,
       columns,
       loading,
       handleTableChange,
-
+      handleQuery,
       edit,
       add,
 
