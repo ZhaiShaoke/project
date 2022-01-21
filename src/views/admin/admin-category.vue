8ed4471c0a00348a72ee3aa2c012b9018f36cf6a
zhai-shaoke <2509551708@qq.com>
2022-01-21 15:03:12 +0800

    feat:tools and methods for adding array to tree data

--- src/views/admin/admin-category.vue
+++ src/views/admin/admin-category.vue
@@ -21,7 +21,7 @@
       <a-table
           :columns="columns"
           :row-key="record => record.id"
-          :data-source="categorys"
+          :data-source="level1"
           :pagination="false"
           :loading="loading"
       >
@@ -110,6 +110,19 @@ export default defineComponent({
       }
     ]
 
+    /**
+     * 一级分类树，children属性就是二级分类
+     * [{
+     *   id: "",
+     *   name: "",
+     *   children: [{
+     *     id: "",
+     *     name: "",
+     *   }]
+     * }]
+     */
+    const level1 = ref() //一级分类树，children属性是二级分类
+
     /*数据查询 */
     const handleQuery = () =>{
       loading.value = true
@@ -118,6 +131,9 @@ export default defineComponent({
         const data = response.data
         /*如果成功的话就出现提示*/
         if(data.code === 200){
+          level1.value = []
+          level1.value = Tool.array2Tree(categorys.value,0)
+          console.log("树形结构：",level1)
           categorys.value = data.data
         }else{
           message.error(data.message)
@@ -176,7 +192,8 @@ export default defineComponent({
 
     return{
       param,
-      categorys,
+      //categorys,
+      level1,
       columns,
       loading,
       handleQuery,
