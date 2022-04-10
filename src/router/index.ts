e26e0d94ef0ebb0ed914963168762e0fc1aa8d57
zhai-shaoke <2509551708@qq.com>
2022-04-10 17:20:56 +0800

    feat:Add routing login interception, and jump back to the routing home page when you are not logged in

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,8 @@ import AdminEbook from '../views/admin/admin-ebook.vue'
 import AdminCategory from '../views/admin/admin-category.vue'
 import AdminDoc from '../views/admin/admin-doc.vue'
 import Doc from '../views/doc.vue'
+import store from "@/store";
+import {Tool} from "../../util/tool";
 
 
 const routes: Array<RouteRecordRaw> = [
@@ -26,22 +28,34 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/admin/user',
     name: 'AdminUser',
-    component: AdminUser
+    component: AdminUser,
+    meta:{
+      loginRequire:true
+    }
   },
   {
     path: '/admin/ebook',
     name: 'AdminEbook',
-    component: AdminEbook
+    component: AdminEbook,
+    meta:{
+      loginRequire:true
+    }
   },
   {
     path: '/admin/category',
     name: 'AdminCategoty',
     component: AdminCategory,
+    meta:{
+      loginRequire:true
+    }
   },
   {
     path: '/admin/doc',
     name: 'AdminDoc',
     component: AdminDoc,
+    meta:{
+      loginRequire:true
+    }
   },
 
 ]
@@ -49,6 +63,25 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+/* 路由登录拦截 逻辑固定在Vue2和Vue3中都支持，可以直接使用，loginRequire是自定义的  */
+router.beforeEach((to,from,next)=>{
+  //要不要对meta.loginRequire属性做监控拦截
+  if(to.matched.some(function(item){
+    console.log(item,"是否需要登录校验：",item.meta.loginRequire)
+    return item.meta.loginRequire
+  })){
+    const loginUser = store.state.user;
+    if(Tool.isEmpty(loginUser)){
+      console.log("用户未登录")
+      next('/')
+    }else{
+      next()
+    }
+  }else{
+    next()
+  }
 })
 
 export default router
